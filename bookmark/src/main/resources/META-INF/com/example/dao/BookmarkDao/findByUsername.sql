a850a63a052c83c267891f17f0241d17904f2c94
Taichi Uragami <backpaper0@gmail.com>
2016-12-24 17:57:53 +0900

    Show count

--- bookmark/src/main/resources/META-INF/com/example/dao/BookmarkDao/findByUsername.sql
+++ bookmark/src/main/resources/META-INF/com/example/dao/BookmarkDao/findByUsername.sql
@@ -1,8 +1,13 @@
     select b.comment
          , e.url
          , e.title
+         , b2.count
       from Bookmark b
 inner join Entry e
         on e.id = b.entryId
+inner join (select entryId, count(*) as count
+              from Bookmark
+          group by entryId) b2
+        on b2.entryId = b.entryId
      where b.username = /* username */'hoge'
 
