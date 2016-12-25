b6190b079fb93af7313ff164bbd7657787c38bbc
Taichi Uragami <backpaper0@gmail.com>
2016-12-25 16:43:55 +0900

    Rename

--- bookmark-api/src/main/java/com/example/BookmarkAPIApplication.java
+++ bookmark-api/src/main/java/com/example/BookmarkAPIApplication.java
@@ -8,9 +8,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @SpringBootApplication
 @EnableResourceServer
 @EnableDiscoveryClient
-public class BookmarkApplication {
+public class BookmarkAPIApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BookmarkApplication.class, args);
+        SpringApplication.run(BookmarkAPIApplication.class, args);
     }
 }
